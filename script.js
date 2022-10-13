46c0f410abc0ed58ac1f6870c2e9500ff74eab4d
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-12 21:53:53 -0700

    access to dropdown menu data now available

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-fetch("https://api.seatgeek.com/2/events?taxonomies.name=nba&per_page=20&client_id=Mjk2NTY3NDJ8MTY2NTUxMzQ3Mi4xMjA1OTg")
-    .then(response => response.json())
-    .then(function (data) {
-        console.log(data);
-    }
-    )
-    .catch(err => console.error(err));
+//https://api.seatgeek.com/2/performers?slug=new-york-mets
+// fetch("https://api.seatgeek.com/2/events?taxonomies.name=nba&per_page=20&client_id=Mjk2NTY3NDJ8MTY2NTUxMzQ3Mi4xMjA1OTg")
+//     .then(response => response.json())
+//     .then(function (data) {
+//         console.log(data);
+//     }
+//     )
+//     .catch(err => console.error(err));
 
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '51d6e0d84cmshc0d9de2b1434d7bp126665jsn3d6cfe1721c5',
-        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-    }
-};
+$(function () {
+    $("#xx").change(function () {
+        var y = parseInt($("#xx option:selected").val());
 
-fetch('https://api-nba-v1.p.rapidapi.com/teams?code=SAC', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '51d6e0d84cmshc0d9de2b1434d7bp126665jsn3d6cfe1721c5',
+                'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+            }
+        };
+
+        fetch('https://api-nba-v1.p.rapidapi.com/teams?id=' + y, options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    })
+})
 
 /*
 ids
