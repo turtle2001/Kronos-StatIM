b0c4205a55d8770afbaf78f7b0d3c84a591e81cb
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-13 16:54:14 -0700

    removed calendar

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 //https://api.seatgeek.com/2/performers?slug=new-york-mets
-// fetch("https://api.seatgeek.com/2/events?taxonomies.name=nba&per_page=20&client_id=Mjk2NTY3NDJ8MTY2NTUxMzQ3Mi4xMjA1OTg")
-//     .then(response => response.json())
-//     .then(function (data) {
-//         console.log(data);
-//     }
-//     )
-//     .catch(err => console.error(err));
+//https://api.seatgeek.com/2/events?q=boston+celtics
 
+var team = "";
 $(function () {
     $('#dropdown').change(function () {
         var teamNum = parseInt($('#dropdown option:selected').val());
@@ -21,128 +16,19 @@ $(function () {
 
         fetch('https://api-nba-v1.p.rapidapi.com/teams?id=' + teamNum, options)
             .then((response) => response.json())
-            .then((response) => console.log(response))
+            .then(function (data) {
+                team = data.response[0].name;
+                fetch("https://api.seatgeek.com/2/events?q=" + team + "&per_page=20&client_id=Mjk2NTY3NDJ8MTY2NTUxMzQ3Mi4xMjA1OTg")
+                    .then(response => response.json())
+                    .then(console.log(data))
+                    .catch(err => console.error(err));
+
+            })
             .catch((err) => console.error(err));
 
         window.location.hash = "schedule";
     });
 });
-
-function generate_year_range(start, end) {
-    var years = "";
-    for (var year = start; year <= end; year++) {
-        years += "<option value='" + year + "'>" + year + "</option>";
-    }
-    return years;
-}
-
-today = new Date();
-currentMonth = today.getMonth();
-currentYear = today.getFullYear();
-selectYear = document.getElementById("year");
-selectMonth = document.getElementById("month");
-
-
-createYear = generate_year_range(currentYear, currentYear + 1);
-
-document.getElementById("year").innerHTML = createYear;
-
-var calendar = document.getElementById("calendar");
-var lang = calendar.getAttribute('data-lang');
-
-var months = "";
-var days = "";
-
-var monthDefault = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-var dayDefault = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-months = monthDefault;
-days = dayDefault;
-
-var $dataHead = "<tr>";
-for (dhead in days) {
-    $dataHead += "<th data-days='" + days[dhead] + "'>" + days[dhead] + "</th>";
-}
-$dataHead += "</tr>";
-
-//alert($dataHead);
-document.getElementById("thead-month").innerHTML = $dataHead;
-
-
-monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
-
-
-
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-function showCalendar(month, year) {
-
-    var firstDay = (new Date(year, month)).getDay();
-
-    tbl = document.getElementById("calendar-body");
-
-
-    tbl.innerHTML = "";
-
-
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    var date = 1;
-    for (var i = 0; i < 6; i++) {
-        var row = document.createElement("tr");
-
-        for (var j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                cell = document.createElement("td");
-                cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            } else if (date > daysInMonth(month, year)) {
-                break;
-            } else {
-                cell = document.createElement("td");
-                cell.setAttribute("data-date", date);
-                cell.setAttribute("data-month", month + 1);
-                cell.setAttribute("data-year", year);
-                cell.setAttribute("data-month_name", months[month]);
-                cell.className = "date-picker";
-                cell.innerHTML = "<span>" + date + "</span>";
-
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.className = "date-picker selected";
-                }
-                row.appendChild(cell);
-                date++;
-            }
-        }
-        tbl.appendChild(row);
-    }
-}
-
-function daysInMonth(iMonth, iYear) {
-    return 32 - new Date(iYear, iMonth, 32).getDate();
-}
 /*
 ids
 navbar
