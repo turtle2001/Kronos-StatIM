ff817dd6394c99d46bb237cd7d4812c7faead7d8
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-13 19:43:56 -0700

    list displayed in correct section

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-//https://api.seatgeek.com/2/performers?slug=new-york-mets
-//https://api.seatgeek.com/2/events?q=boston+celtics
-
 $(function () {
     $('#dropdown').change(function () {
         var teamNum = parseInt($('#dropdown option:selected').val());
@@ -18,15 +14,17 @@ $(function () {
             .then((response) => response.json())
             .then(function (data) {
                 team = data.response[0].name;
-                console.log(team)
-                fetch("https://api.seatgeek.com/2/events?q=" + team + "&per_page=20&client_id=Mjk2NTY3NDJ8MTY2NTUxMzQ3Mi4xMjA1OTg")
+                fetch("https://api.seatgeek.com/2/events?q=" + team + "&per_page=82&client_id=Mjk2NTY3NDJ8MTY2NTUxMzQ3Mi4xMjA1OTg")
                     .then(response2 => response2.json())
                     .then(function (data2) {
-                        for (var i = 0; i < data2.events.length; i++) {
-                            console.log(data2.events[i].title)
+                        //console.log(data2)
+                        $("#list").empty();
+                        for (var i = 0; i < 5; i++) {
+                            var teamEl = $("<li></li>").text(data2.events[i].title);
+                            $("#list").append(teamEl);
                         }
-                        console.log(data2)
-                    })
+                    }
+                    )
                     .catch(err2 => console.error(err2));
 
             })
@@ -38,58 +36,42 @@ $(function () {
 
 function openModal() {
     document.getElementById('myModal').style.display = "block";
-  }
-  
-  function closeModal() {
+}
+
+function closeModal() {
     document.getElementById('myModal').style.display = "none";
-  }
-  
-  var slideIndex = 1;
-  showDivs(slideIndex);
-  
-  function plusDivs(n) {
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
     showDivs(slideIndex += n);
-  }
-  
-  function currentDiv(n) {
+}
+
+function currentDiv(n) {
     showDivs(slideIndex = n);
-  }
-  
-  function showDivs(n) {
+}
+
+function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
     }
-    x[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " w3-opacity-off";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-opacity-off";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
 
-
-
-/*
-ids
-navbar
-main
-home
-teams
-schedule
-tickets
-photos
-contact
-
-
-
-SEATGEEK APIs
-data[0].title - Name of Event
-data[0].datetime_local -Time of Event
-data[0].url (for tickets) - Link for tickets
-*/
+// SEATGEEK APIs
+// data[0].title - Name of Event
+// data[0].datetime_local -Time of Event
+// data[0].url (for tickets) - Link for tickets
